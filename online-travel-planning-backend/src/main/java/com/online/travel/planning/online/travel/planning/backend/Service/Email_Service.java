a2d0caa82b6eb175cbefaa4c9327aeb2083bc65e
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-14 13:32:15 +0530

    Mime Message

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/Email_Service.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/Email_Service.java
@@ -4,11 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import jakarta.mail.internet.MimeMessage;
+
 @Service
 public class Email_Service {
     @Autowired
  private JavaMailSender mailSender; // Use JavaMailSender instead of MailSender
  public void sendWelcomeEmail(String toEmail, String subject, String htmlContent) {
+try {
+            // Create a MimeMessage
+            MimeMessage message = mailSender.createMimeMessage();
 
 
+ }
+}
 }
