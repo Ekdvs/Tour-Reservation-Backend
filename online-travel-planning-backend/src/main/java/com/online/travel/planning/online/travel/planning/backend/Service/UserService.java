5ad63a6d4bb2dfa0d9c1d1f77d8ffbdfa27bf972
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-14 13:03:10 +0530

    Create User

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/UserService.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/UserService.java
@@ -1,4 +1,10 @@
 package com.online.travel.planning.online.travel.planning.backend.Service;
 
+import com.online.travel.planning.online.travel.planning.backend.Model.User;
+
+import jakarta.mail.MessagingException;
+
 public interface UserService {
+    User createUser(User user) throws MessagingException;
+
 }
