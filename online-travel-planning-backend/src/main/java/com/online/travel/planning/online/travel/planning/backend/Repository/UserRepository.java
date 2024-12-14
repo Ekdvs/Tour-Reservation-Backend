27cd6b85f95d6891fbf84daa533d0e2fbfbe0263
Ekdvs <ekdvsampath02@gmail.com>
2024-12-14 13:27:15 +0530

    create class and interface extends to mongo db

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/UserRepository.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.online.travel.planning.online.travel.planning.backend.Repository;
+import com.online.travel.planning.online.travel.planning.backend.Model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public class UserRepository {
+public class UserRepository extends MongoRepository<User,String> {
+
+
 
 }
