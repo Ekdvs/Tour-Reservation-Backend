75c544e7756f661f996c365583ed9a430a273e79
Ekdvs <ekdvsampath02@gmail.com>
2024-12-14 16:16:05 +0530

    implementation userservice

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/UserRepository.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.online.travel.planning.online.travel.planning.backend.Repository;
 
 import com.online.travel.planning.online.travel.planning.backend.Model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository extends MongoRepository<User,String> {
     Optional<User> findByUserId(String userId);
     User findByUserEmail(String username);
     Optional<User> findUsernameByUserId(String userId);
