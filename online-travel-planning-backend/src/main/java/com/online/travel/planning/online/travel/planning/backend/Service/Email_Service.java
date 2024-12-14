1ded830dc1b6c1413d106e67fd6a50f0e4210608
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-14 13:27:24 +0530

    Autowired

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/Email_Service.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/Email_Service.java
@@ -1,8 +1,10 @@
 package com.online.travel.planning.online.travel.planning.backend.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Email_Service {
+    @Autowired
 
 }
