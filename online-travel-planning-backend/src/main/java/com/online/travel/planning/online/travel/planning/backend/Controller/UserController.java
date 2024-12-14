6cae165ffbfcee77ed3c71172cbfc8e21f90949f
IsharaJJ <ijjayawardhana46367@gmail.com>
2024-12-14 13:15:24 +0530

    Added imports to UserController class

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/UserController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/UserController.java
@@ -1,4 +1,18 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
+import com.online.travel.planning.online.travel.planning.backend.Model.User;
+import com.online.travel.planning.online.travel.planning.backend.Repository.UserRepository;
+import com.online.travel.planning.online.travel.planning.backend.Service.UserService;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/user")
 
 public class UserController {
 
