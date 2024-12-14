5e1f91d78b0790e06983b44d75fa0d5ef6a3b4f9
Ekdvs <ekdvsampath02@gmail.com>
2024-12-14 16:09:49 +0530

    update user class

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/User.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/User.java
@@ -1,6 +1,31 @@
 package com.online.travel.planning.online.travel.planning.backend.Model;
 
-public class User {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-    String name;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Document(collection = "user")
+public class User {
+    @Id
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String userEmail;
+    private String password;
+    private String phoneNumber;
+    private String userRole="user";
+    private String title;
+    private String gender;
+    private String country;
+
+    private LocalDate dateRegistered = LocalDate.now();
+    private LocalTime timeRegistered = LocalTime.now();
+
+
 }
