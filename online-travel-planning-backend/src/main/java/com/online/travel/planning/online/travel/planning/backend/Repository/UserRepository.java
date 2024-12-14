fd74adcaa031d9090c654bcd46223f67ce7663db
Ekdvs <ekdvsampath02@gmail.com>
2024-12-14 16:13:22 +0530

    findUsernameByUserId

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/UserRepository.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.online.travel.planning.online.travel.planning.backend.Repository;
 
+import com.online.travel.planning.online.travel.planning.backend.Model.User;
+
+import java.util.Optional;
+
 public interface UserRepository {
+    Optional<User> findByUserId(String userId);
+    User findByUserEmail(String username);
+    Optional<User> findUsernameByUserId(String userId);
 }
