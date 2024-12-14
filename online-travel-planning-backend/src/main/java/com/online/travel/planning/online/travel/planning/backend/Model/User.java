f5be31edb705c98a5b01be028f0a73894783a948
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-14 16:49:26 +0530

    Document

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/User.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/User.java
@@ -1,10 +1,14 @@
 package com.online.travel.planning.online.travel.planning.backend.Model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Document(collection = "user")
+
 public class User {
 
 }
