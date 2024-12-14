14d9b0b79eaaaccc9c9aa25405f612081af05613
Ekdvs <ekdvsampath02@gmail.com>
2024-12-14 16:39:37 +0530

    map to recory code

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/UserServiceImplementation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/UserServiceImplementation.java
@@ -2,6 +2,10 @@ package com.online.travel.planning.online.travel.planning.backend.ServiceImpleme
 
 import com.online.travel.planning.online.travel.planning.backend.Repository.UserRepository;
 import com.online.travel.planning.online.travel.planning.backend.Service.UserService;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +16,7 @@ public class UserServiceImplementation implements UserService {
 
    /*  @Autowired
     private EmailService emailService;*/
+
+    private final Map<String,String> recoveryCodes = new HashMap<>();
 
 }
