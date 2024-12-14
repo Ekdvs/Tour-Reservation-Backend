396af47386da6644ab43905d736856fbe1c269b7
Ekdvs <ekdvsampath02@gmail.com>
2024-12-14 16:15:07 +0530

    implementation userservice

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/UserServiceImplementation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/UserServiceImplementation.java
@@ -1,8 +1,9 @@
 package com.online.travel.planning.online.travel.planning.backend.ServiceImplementation;
 
+import com.online.travel.planning.online.travel.planning.backend.Service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImplementation {
+public class UserServiceImplementation implements UserService {
 
 }
