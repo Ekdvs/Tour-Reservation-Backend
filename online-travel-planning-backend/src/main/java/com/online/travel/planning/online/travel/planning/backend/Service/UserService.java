a5c5595e382a4b31d9452f527ea9cc90cef0a81b
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-14 13:10:16 +0530

    List

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/UserService.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/UserService.java
@@ -1,5 +1,7 @@
 package com.online.travel.planning.online.travel.planning.backend.Service;
 
+import java.util.List;
+
 import com.online.travel.planning.online.travel.planning.backend.Model.User;
 
 import jakarta.mail.MessagingException;
@@ -9,5 +11,6 @@ public interface UserService {
     User getUserById(String userId);
     User getUserNameById(String userId);
     User getUserByUserEmail(String userEmail);
+List<User> getAllUsers();
 
 }
