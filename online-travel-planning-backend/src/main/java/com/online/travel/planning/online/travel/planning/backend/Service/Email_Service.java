1b6d1109e8a42afcad03afa95ba4baf59b25e81d
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-14 13:28:38 +0530

    Mail Sender

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/Email_Service.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/Email_Service.java
@@ -1,10 +1,13 @@
 package com.online.travel.planning.online.travel.planning.backend.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Email_Service {
     @Autowired
+ private JavaMailSender mailSender; // Use JavaMailSender instead of MailSender
+
 
 }
