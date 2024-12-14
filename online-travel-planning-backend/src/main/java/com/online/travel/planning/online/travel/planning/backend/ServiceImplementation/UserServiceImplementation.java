d862690afdad9c5a82e8160d89ff884765fad6c8
Ekdvs <ekdvsampath02@gmail.com>
2024-12-14 16:14:28 +0530

    service

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/UserServiceImplementation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/UserServiceImplementation.java
@@ -1,4 +1,8 @@
 package com.online.travel.planning.online.travel.planning.backend.ServiceImplementation;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImplementation {
+
 }
