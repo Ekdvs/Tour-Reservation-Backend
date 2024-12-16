a3fbcf5593f0f3953c92822248a776b0625a3108
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-16 07:59:50 +0530

    update local host

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/UserController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/UserController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:3001")
 @RequestMapping("/user")
 public class UserController {
 
