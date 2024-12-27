af5952094f70ad9c0948e8363ac9934c242f3e2b
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:36:00 +0530

    getReservationById

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/ReservationServiceImplementation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/ReservationServiceImplementation.java
@@ -1,6 +1,7 @@
 package com.online.travel.planning.online.travel.planning.backend.ServiceImplementation;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,11 @@ public class ReservationServiceImplementation
     @Override
     public List<Reservation> getReservationByUserId(String userId){
         return reservationRepository.findReservationByUserId(userId);
-    };
+    }
+
+    @Override
+    public Optional<Reservation> getReservationById(String reservationId) {
+        return reservationRepository.findById(reservationId);
+    }
 
 }
