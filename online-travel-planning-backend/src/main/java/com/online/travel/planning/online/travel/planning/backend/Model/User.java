d170a9d6a56e8f09a661e220a89ab603904a9ff9
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 09:42:44 +0530

    update user

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/User.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/User.java
@@ -1,6 +1,7 @@
 package com.online.travel.planning.online.travel.planning.backend.Model;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 import org.springframework.data.annotation.Id;
@@ -25,104 +26,9 @@ public class User {
     private String title;
     private String gender;
     private String country;
-
+    private String profilePictureUrl;
     private LocalDate dateRegistered =LocalDate.now();
     private LocalTime timeRegistered =LocalTime.now();
+    private LocalDateTime lastLogin;
 
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(String userRole) {
-        this.userRole = userRole;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public LocalDate getDateRegistered() {
-        return dateRegistered;
-    }
-
-    public void setDateRegistered(LocalDate dateRegistered) {
-        this.dateRegistered = dateRegistered;
-    }
-
-    public LocalTime getTimeRegistered() {
-        return timeRegistered;
-    }
-
-    public void setTimeRegistered(LocalTime timeRegistered) {
-        this.timeRegistered = timeRegistered;
-    }
-}
+   }
