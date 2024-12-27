93b84c16ca2b253c7b22c4c4c57b9a317fa1ae3a
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:41:17 +0530

    getReservationById

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/ReservationController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/ReservationController.java
@@ -1,10 +1,12 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,11 @@ public class ReservationController {
     @GetMapping("/getAllReservations")
     public List<Reservation> getAllReservations() {
         return reservationService.getAllReservations();
+    }
+
+     @GetMapping("/getReservationById/{id}")
+    public Optional<Reservation> getReservationById(@PathVariable("id") String reservationId) {
+        return reservationService.getReservationById(reservationId);
     }
 
     
