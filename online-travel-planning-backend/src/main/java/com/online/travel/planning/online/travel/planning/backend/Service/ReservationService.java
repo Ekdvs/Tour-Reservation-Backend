be4e16d8ccc73ee61e5cd2bf9d26ce8c57722871
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:26:45 +0530

    getAllReservations

---
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/ReservationService.java
@@ -0,0 +1,9 @@
+package com.online.travel.planning.online.travel.planning.backend.Service;
+
+import java.util.List;
+
+import com.online.travel.planning.online.travel.planning.backend.Model.Reservation;
+
+public interface ReservationService {
+    List<Reservation> getAllReservations();
+}
