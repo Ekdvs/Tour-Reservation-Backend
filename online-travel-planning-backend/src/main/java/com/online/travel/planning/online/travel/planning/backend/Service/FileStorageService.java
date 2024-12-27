ea5872db6158c5b288001e7e3e030b894ccf7372
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:50:18 +0530

    final string

---
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/FileStorageService.java
@@ -0,0 +1,6 @@
+package com.online.travel.planning.online.travel.planning.backend.Service;
+
+public class FileStorageService {
+    private final String uploadDir = "uploads/profile_pictures/";
+
+}
