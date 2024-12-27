3dd8df3676304e0b95c28da5d6f5ba03d8a98426
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:42:23 +0530

    updateReservation

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/ReservationController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/ReservationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,6 +47,12 @@ public class ReservationController {
     @PostMapping("/addReservation")
     public Reservation addReservation(@RequestBody Reservation reservation) {
         return reservationService.addReservation(reservation);
+    }
+
+    @PutMapping("/updateReservation/{id}")
+    public Reservation updateReservation(@PathVariable("id") String reservationId,
+                                         @RequestBody Reservation reservation) {
+        return reservationService.updateReservation(reservationId, reservation);
     }
 
     
