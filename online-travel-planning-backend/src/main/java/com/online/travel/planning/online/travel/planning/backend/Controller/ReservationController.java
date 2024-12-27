4f34c52a5caa0d26b665462d968d16a35b1c0c6a
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:40:22 +0530

    @Autowired

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/ReservationController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/ReservationController.java
@@ -1,12 +1,18 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.online.travel.planning.online.travel.planning.backend.Service.ReservationService;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 @RequestMapping("/reservation")
 public class ReservationController {
+
+    @Autowired
+    private ReservationService reservationService;
 
 }
