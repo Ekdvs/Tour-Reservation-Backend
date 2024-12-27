e1e12b600cc16c613ed798047344117325d19df5
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:18:50 +0530

    add id

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/Reservation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/Reservation.java
@@ -1,8 +1,11 @@
 package com.online.travel.planning.online.travel.planning.backend.Model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "reservation")
 public class Reservation {
+    @Id
+    private String reservationId;
 
 }
