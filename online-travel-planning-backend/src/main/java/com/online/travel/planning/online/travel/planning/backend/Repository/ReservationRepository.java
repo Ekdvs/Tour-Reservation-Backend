6af1dda49750b3e2970811500b27bbe2e729a9ed
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:24:44 +0530

    findTotalChargesByDate

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/ReservationRepository.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/ReservationRepository.java
@@ -1,13 +1,19 @@
 package com.online.travel.planning.online.travel.planning.backend.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.online.travel.planning.online.travel.planning.backend.Model.Reservation;
 
 public interface ReservationRepository extends MongoRepository <Reservation, String>{
 
      List<Reservation> findReservationByUserId(String userId);
+      @Query(value = "{'reservationDate': ?0}", fields = "{'totalCharge': 1}")
+    List<Reservation> findTotalChargesByDate(LocalDate date);
+
+   
 
 }
