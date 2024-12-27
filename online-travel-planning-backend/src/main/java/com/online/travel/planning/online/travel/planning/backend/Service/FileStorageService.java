8456b00c9b03b399f80e9ff3084329a339476c2a
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:51:04 +0530

    saveFile

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/FileStorageService.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/FileStorageService.java
@@ -1,6 +1,18 @@
 package com.online.travel.planning.online.travel.planning.backend.Service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public class FileStorageService {
     private final String uploadDir = "uploads/profile_pictures/";
+
+    public String saveFile(MultipartFile file) {
+        
+    }
 
 }
