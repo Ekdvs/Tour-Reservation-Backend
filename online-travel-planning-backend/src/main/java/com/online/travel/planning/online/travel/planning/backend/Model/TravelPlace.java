dabbceb47d13c7ef641bfd9f4885888d22818c84
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-27 16:41:18 +0530

    Add placeName and location fields

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/TravelPlace.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/TravelPlace.java
@@ -3,6 +3,8 @@ package com.online.travel.planning.online.travel.planning.backend.Model;
 public class TravelPlace {
     @Id
     private String placeId;
-    
+    private String placeName;
+    private String location;
+
 
 }
