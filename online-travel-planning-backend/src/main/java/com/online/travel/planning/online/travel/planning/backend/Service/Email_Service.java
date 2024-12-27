26f04e24124f67a006f149b00000419f747276d6
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 09:58:06 +0530

    promoteUserToGuide

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/Email_Service.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/Email_Service.java
@@ -1,72 +1,29 @@
 package com.online.travel.planning.online.travel.planning.backend.Service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-
 @Service
 public class Email_Service {
     @Autowired
- private JavaMailSender mailSender;
- public void sendWelcomeEmail(String toEmail, String subject, String htmlContent) {
-try {
-
-            MimeMessage message = mailSender.createMimeMessage();
- 
-
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(toEmail);
-            helper.setSubject(subject);
-            helper.setText(htmlContent, true); // Pass 'true' for HTML content
-            helper.setFrom("ceylontravelplanning@gmail.com");
-
-
-            mailSender.send(message);
-        }
-         catch (MessagingException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Failed to send email");
-        }
- }
-
-
-    public void sendotpcode(String toEmail, String subject, String htmlContent) {
-                try {
-
-                    MimeMessage message = mailSender.createMimeMessage();
-                
-                 // Use MimeMessageHelper to configure the email
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(toEmail);
-            helper.setSubject(subject);
-            helper.setText(htmlContent, true); // Pass 'true' for HTML content
-            helper.setFrom("ceylontravelplanning@gmail.com");
-
-            // Send the email
-            mailSender.send(message);
-        } 
-        catch (MessagingException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Failed to send email");
-                }
-            }
-    public void updatepassword(String toEmail, String subject, String htmlContent) {
+    private JavaMailSender mailSender;
+    public void sendEmail(String toEmail, String subject, String htmlContent) {
         try {
 
             MimeMessage message = mailSender.createMimeMessage();
 
-            // Use MimeMessageHelper to configure the email
+
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
             helper.setTo(toEmail);
             helper.setSubject(subject);
             helper.setText(htmlContent, true); // Pass 'true' for HTML content
             helper.setFrom("ceylontravelplanning@gmail.com");
 
-            // Send the email
+
             mailSender.send(message);
         }
         catch (MessagingException e) {
@@ -74,7 +31,32 @@ try {
             throw new RuntimeException("Failed to send email");
         }
     }
+    public void sendOTPEmail(String toEmail, String subject, String htmlContent) {
+        try {
 
- }
+            MimeMessage message = mailSender.createMimeMessage();
+
+
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(toEmail);
+            helper.setSubject(subject);
+            helper.setText(htmlContent, true); // Pass 'true' for HTML content
+            helper.setFrom("ceylontravelplanning@gmail.com");
+
+
+            mailSender.send(message);
+        }
+
+
+        catch (MessagingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to send email");
+        }
+    }
+
+
+
+
+}
 
 
