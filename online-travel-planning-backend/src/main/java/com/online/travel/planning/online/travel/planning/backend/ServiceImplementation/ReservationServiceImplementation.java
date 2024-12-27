30a144fd80e272c77e6a2b8a05d6e8e3f9f33d89
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:32:55 +0530

    @Service

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/ReservationServiceImplementation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/ReservationServiceImplementation.java
@@ -1,5 +1,10 @@
 package com.online.travel.planning.online.travel.planning.backend.ServiceImplementation;
 
+import org.springframework.stereotype.Service;
+
+import io.swagger.v3.oas.annotations.servers.Servers;
+
+@Service
 public class ReservationServiceImplementation {
 
 }
