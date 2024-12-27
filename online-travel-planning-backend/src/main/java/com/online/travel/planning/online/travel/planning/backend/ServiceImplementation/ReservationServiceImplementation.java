a7f49e52b06064108f919cc5cb786361ae9915f8
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:34:12 +0530

    reservationRepository

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/ReservationServiceImplementation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/ReservationServiceImplementation.java
@@ -1,12 +1,17 @@
 package com.online.travel.planning.online.travel.planning.backend.ServiceImplementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.online.travel.planning.online.travel.planning.backend.Repository.ReservationRepository;
 import com.online.travel.planning.online.travel.planning.backend.Service.ReservationService;
 
 import io.swagger.v3.oas.annotations.servers.Servers;
 
 @Service
-public class ReservationServiceImplementation implements ReservationService{
+public class ReservationServiceImplementation 
+
+@Autowired
+    private ReservationRepository reservationRepository;
 
 }
