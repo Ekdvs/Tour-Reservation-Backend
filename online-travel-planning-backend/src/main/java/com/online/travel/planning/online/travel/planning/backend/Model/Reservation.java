98cbbbdeb9b61b3a81ec66df7cf5585dc10b63f2
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:17:46 +0530

    add document

---
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/Reservation.java
@@ -0,0 +1,8 @@
+package com.online.travel.planning.online.travel.planning.backend.Model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reservation")
+public class Reservation {
+
+}
