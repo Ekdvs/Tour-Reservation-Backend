61de556fa8a509f7023db47c8d1e3763ba3e2b8a
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 10:24:27 +0530

    findReservationByUserId

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/ReservationRepository.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/ReservationRepository.java
@@ -1,9 +1,13 @@
 package com.online.travel.planning.online.travel.planning.backend.Repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.online.travel.planning.online.travel.planning.backend.Model.Reservation;
 
 public interface ReservationRepository extends MongoRepository <Reservation, String>{
+
+     List<Reservation> findReservationByUserId(String userId);
 
 }
