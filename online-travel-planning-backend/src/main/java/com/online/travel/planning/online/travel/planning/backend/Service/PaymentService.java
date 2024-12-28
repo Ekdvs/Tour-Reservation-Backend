2b5ea8c9e0701c71483d999c7a668b88997939d8
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 10:20:41 +0530

    add methods to retrieve and delete payments

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/PaymentService.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/PaymentService.java
@@ -3,6 +3,9 @@ package com.online.travel.planning.online.travel.planning.backend.Service;
 public interface PaymentService {
     Payment processPayment(Payment payment);
     void sendPaymentSuccessEmail(Payment payment);
+    Optional<Payment> getPaymentById(String paymentId);
+    void deletePayment(String paymentId);
+
 
 
 }
