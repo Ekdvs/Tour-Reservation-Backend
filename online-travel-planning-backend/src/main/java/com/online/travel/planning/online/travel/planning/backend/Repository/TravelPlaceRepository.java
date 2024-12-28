4fd0bed79db6d128fd9b5aeb34dcbd8a05753927
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 08:02:15 +0530

    add method to find TravelPlace by place name in TravelPlaceRepository

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/TravelPlaceRepository.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/TravelPlaceRepository.java
@@ -3,5 +3,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.online.travel.planning.online.travel.planning.backend.Model.TravelPlace;
 
 public interface TravelPlaceRepository extends MongoRepository <TravelPlace,String> {
+    //Optional<TravelPlace> findByTravelPlaceName(String travelPlacename);
+   // TravelPlace findByTravelPlaceName(String travelPlaceName);
+   // Optional<TravelPlace>findTravelPlaceByUserId(String userId);
+   TravelPlace findByPlaceName(String PlaceName);
+
 
 }
