80c9daad772e94ed46d4e60e733a601dc1095ec2
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 07:52:30 +0530

    add MongoDB annotations to TravelPlace model

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/TravelPlace.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/TravelPlace.java
@@ -1,4 +1,7 @@
 package com.online.travel.planning.online.travel.planning.backend.Model;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 
 public class TravelPlace {
     @Id
