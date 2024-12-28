44fa074c37d65d6c2211a99ced3cae17c5d0876d
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 08:44:08 +0530

    CrossOrigin and RequestMapping annotations to TravelPlaceController

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/TravelPlaceController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/TravelPlaceController.java
@@ -1,5 +1,7 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
 @RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/place")
 public class TravelPlaceController {
     @Autowired
     private TravelPlaceService travelPlaceService;
