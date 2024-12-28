5e7c3017bc3eb12402993470c8b7f28959a2878a
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 08:41:53 +0530

    @RestController annotation to TravelPlaceController

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/TravelPlaceController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/TravelPlaceController.java
@@ -1,5 +1,5 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
-
+@RestController
 public class TravelPlaceController {
     @Autowired
     private TravelPlaceService travelPlaceService;
