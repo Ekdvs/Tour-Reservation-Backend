cc8accf8c0a3f56cfc9045c79f53cac33201f391
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 10:19:50 +0530

    methods for processing payments and sending success emails

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/PaymentService.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/PaymentService.java
@@ -1,5 +1,8 @@
 package com.online.travel.planning.online.travel.planning.backend.Service;
 
 public interface PaymentService {
+    Payment processPayment(Payment payment);
+    void sendPaymentSuccessEmail(Payment payment);
+
 
 }
