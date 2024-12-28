7444073e2ab552b31d28c198832e6d24d4b0381e
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 09:13:37 +0530

    add List import and MongoDB annotations

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/Payment.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/Payment.java
@@ -1,6 +1,10 @@
 package com.online.travel.planning.online.travel.planning.backend.Model;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 
 @Document(collection = "payment")
 public class Payment {
