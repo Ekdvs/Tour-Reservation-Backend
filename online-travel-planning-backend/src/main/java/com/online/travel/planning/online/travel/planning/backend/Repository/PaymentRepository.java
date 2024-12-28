8cb2f48c173051dc6ae799f87db633666c47793f
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 10:03:39 +0530

    add aggregation method to PaymentRepository for daily income calculation

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/PaymentRepository.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/PaymentRepository.java
@@ -3,6 +3,10 @@ package com.online.travel.planning.online.travel.planning.backend.Repository;
 public interface PaymentRepository extends MongoRepository<Payment,String>{
     List<Payment> findByUserId(String userId);
     List<Payment> findByReservationId(String reservationId);
-
+    @Aggregation(pipeline = {
+        "{ '$group': { '_id': '$paymentDate', 'totalIncome': { '$sum': '$amount' } } }",
+        "{ '$project': { 'date': '$_id', 'totalIncome': 1, '_id': 0 } }"
+})
+List<DailyIncome> aggregateDailyIncome();
 
 }
