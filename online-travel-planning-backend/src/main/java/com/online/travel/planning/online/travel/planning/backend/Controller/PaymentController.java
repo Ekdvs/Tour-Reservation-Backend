6ca39fb59007a19229767204e305dfc25c048278
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 12:58:04 +0530

    refactor PaymentController to update import paths and enhance code formatting

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/PaymentController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/PaymentController.java
@@ -1,11 +1,13 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
+
 import com.online.travel.planning.online.travel.planning.backend.Model.DailyIncome;
 import com.online.travel.planning.online.travel.planning.backend.Model.Payment;
 import com.online.travel.planning.online.travel.planning.backend.Repository.PaymentRepository;
-import com.online.travel.planning.online.travel.planning.backend.Service.PaymentServiceImplementation;
+import com.online.travel.planning.online.travel.planning.backend.ServiceImplementation.PaymentServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -15,8 +17,10 @@ import java.util.Optional;
 public class PaymentController {
     @Autowired
     private PaymentServiceImplementation paymentService;
+
     @Autowired
     private PaymentRepository paymentRepository;
+
     public PaymentController(PaymentServiceImplementation paymentService) {
         this.paymentService = paymentService;
     }
@@ -25,30 +29,35 @@ public class PaymentController {
         List<DailyIncome> dailyIncome = paymentRepository.aggregateDailyIncome(); // Implement aggregation in your repository
         return ResponseEntity.ok(dailyIncome);
     }
+
     @PostMapping("/process") // add method
     public Payment processPayment(@RequestBody Payment payment) {
         return paymentService.processPayment(payment);
     }
+
     @GetMapping("/getPaymentByUserId/{id}")
     public List<Payment> getPaymentByUserId(@PathVariable("id") String userId) {
         return paymentService.getPaymentByUserId(userId);
     }
+
     @GetMapping("/getPaymentByReservationId/{id}")
     public List<Payment> getPaymentByReservationId(@PathVariable("id") String reservationId) {
         return paymentService.getPaymentByReservationId(reservationId);
     }
+
     @GetMapping("/getAllPayment")
     public List<Payment> getAllPayment() {
         return paymentService.getAllPayment();
     }
+
     @GetMapping("/getPaymentById/{id}")
     public Optional<Payment> getPaymentById(@PathVariable("id") String paymentId) {
         return paymentService.getPaymentById(paymentId);
     }
+
     @DeleteMapping("/deletePayment/{id}")
     public String deletePayment(@PathVariable("id") String paymentId) {
         paymentService.deletePayment(paymentId);
         return "Payment deleted with id " + paymentId;
     }
-
 }
