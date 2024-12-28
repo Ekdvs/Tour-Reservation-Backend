9541a46ed32763d22f383ea6346e08081cc076a5
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 08:00:46 +0530

    extend TravelPlaceRepository to inherit from MongoRepository for CRUD operations

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/TravelPlaceRepository.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/TravelPlaceRepository.java
@@ -1,5 +1,7 @@
 package com.online.travel.planning.online.travel.planning.backend.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.online.travel.planning.online.travel.planning.backend.Model.TravelPlace;
 
-public interface TravelPlaceRepository {
+public interface TravelPlaceRepository extends MongoRepository <TravelPlace,String> {
 
 }
