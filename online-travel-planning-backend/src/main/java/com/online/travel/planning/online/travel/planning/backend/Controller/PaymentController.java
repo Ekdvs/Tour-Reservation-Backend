6fd2bd445c52b25fd40090e20981373a89c31338
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 10:48:32 +0530

    annotations for PaymentController to enable REST functionality

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/PaymentController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/PaymentController.java
@@ -1,5 +1,7 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
-
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/payment")
 public class PaymentController {
 
 }
