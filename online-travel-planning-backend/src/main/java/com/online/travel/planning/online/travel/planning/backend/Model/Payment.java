2e0d260bdf90334b7b3d64b37ca78d63e46d6d3f
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 09:12:47 +0530

    LocalDate and LocalTime imports to Payment model

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/Payment.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/Payment.java
@@ -1,4 +1,7 @@
 package com.online.travel.planning.online.travel.planning.backend.Model;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Document(collection = "payment")
 public class Payment {
 
