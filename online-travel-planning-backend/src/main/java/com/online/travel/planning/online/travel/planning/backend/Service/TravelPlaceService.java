17f6f7fb0649749f13dc3c5b15fabe1719ce6046
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-28 08:05:08 +0530

    add TravelPlace import to TravelPlaceService interface

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/TravelPlaceService.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/TravelPlaceService.java
@@ -1,4 +1,5 @@
 package com.online.travel.planning.online.travel.planning.backend.Service;
+import com.online.travel.planning.online.travel.planning.backend.Model.TravelPlace;
 
 public interface TravelPlaceService {
 
