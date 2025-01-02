e8b1124fca5ab9e4ebfbafa113ced8b342ddc2d0
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:08:35 +0530

    findByEventNameContainingIgnoreCase

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/EventRepository.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/EventRepository.java
@@ -10,5 +10,6 @@ import com.online.travel.planning.online.travel.planning.backend.Model.Event;
 public interface EventRepository extends MongoRepository<Event, String>{
      @Query("{ 'eventType' : ?0 }")
     List<Event> findByType(String eventType);
+    public List<Event> findByEventNameContainingIgnoreCase(String name);
 
 }
