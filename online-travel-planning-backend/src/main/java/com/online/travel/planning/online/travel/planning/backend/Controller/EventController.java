0b2faec2eb67e34dd29232cebb85405eaba932ed
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:36:41 +0530

    add addEvent method to EventController for handling event creation with image upload

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/EventController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/EventController.java
@@ -1,17 +1,23 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.online.travel.planning.online.travel.planning.backend.Model.Event;
 import com.online.travel.planning.online.travel.planning.backend.Service.EventService;
 
@@ -44,6 +50,12 @@ public class EventController {
     public List<Event> getEventByType(@PathVariable("eventType") String eventType) {
         return eventService.getEventByType(eventType);
     }
+
+    @PostMapping("/addEvent")
+    public ResponseEntity<?>addEvent(@RequestPart("event")String eventJson,@RequestPart("imageFile") MultipartFile imagefile) throws IOException {
+        
+    }
+
 
 
 
