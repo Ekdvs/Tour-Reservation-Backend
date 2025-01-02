4a70b244432f4db97dd802830f5c6bc8908c0b39
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 14:06:20 +0530

    add PackagesController class to handle package-related requests

---
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/PackagesController.java
@@ -0,0 +1,8 @@
+package com.online.travel.planning.online.travel.planning.backend.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PackagesController {
+
+}
