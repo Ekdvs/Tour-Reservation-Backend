4822adb5f9a2a2a6e486d4fcddb5e3601fcd0b01
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:33:40 +0530

    add @RequestMapping annotation to EventController for event endpoint mapping

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/EventController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/EventController.java
@@ -1,10 +1,12 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
+@RequestMapping("/event")
 public class EventController {
 
 }
