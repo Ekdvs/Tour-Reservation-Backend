a4f1ef6ba04ac3df3c8f37c67364f395e58d98fb
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:59:02 +0530

    add PackagesRepository dependency to PackagesServiceImplementation

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/PackagesServiceImplementation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/PackagesServiceImplementation.java
@@ -1,8 +1,14 @@
 package com.online.travel.planning.online.travel.planning.backend.ServiceImplementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.online.travel.planning.online.travel.planning.backend.Repository.PackagesRepository;
 
 @Service
 public class PackagesServiceImplementation implements PackagesService {
+
+    @Autowired
+    private PackagesRepository packagesRepository;
 
 }
