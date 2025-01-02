0c2802b749d97ee998f8cf95c2cc11fce651ee40
Ekdvs <ekdvsampath02@gmail.com>
2025-01-02 14:12:32 +0530

    feat: add TravelPlaceController with basic setup and imports

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/TravelPlaceController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/TravelPlaceController.java
@@ -1,4 +1,18 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.online.travel.planning.online.travel.planning.backend.Model.TravelPlace;
+import com.online.travel.planning.online.travel.planning.backend.Service.TravelPlaceService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 @RestController
 @CrossOrigin("http://localhost:3000")
 @RequestMapping("/place")
