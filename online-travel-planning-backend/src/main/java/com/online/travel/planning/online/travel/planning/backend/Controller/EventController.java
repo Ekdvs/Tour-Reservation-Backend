e401bd690e856978cba98e7368ab9209db77458d
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:35:21 +0530

    add searchEvents endpoint to EventController for event searching functionality

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/EventController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/EventController.java
@@ -3,9 +3,11 @@ package com.online.travel.planning.online.travel.planning.backend.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.online.travel.planning.online.travel.planning.backend.Model.Event;
@@ -24,5 +26,9 @@ public class EventController {
     public List<Event> getAllEvents() {
         return eventService.getAllEvents();
     }
+
+    @GetMapping("/searchEvents")
+    
+
 
 }
