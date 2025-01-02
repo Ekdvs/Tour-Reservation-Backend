c33a59b4ec32e174f547deb2e3744f96224f22d7
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:15:25 +0530

    implements

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/EventServiceImplementation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/EventServiceImplementation.java
@@ -1,5 +1,7 @@
 package com.online.travel.planning.online.travel.planning.backend.ServiceImplementation;
 
-public class EventServiceImplementation {
+import com.online.travel.planning.online.travel.planning.backend.Service.EventService;
+
+public class EventServiceImplementation implements EventService{
 
 }
