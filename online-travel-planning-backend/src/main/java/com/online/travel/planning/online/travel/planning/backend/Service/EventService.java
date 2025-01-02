f750d3195718b83bf79a8968be97f968591c7ce4
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:10:31 +0530

    getAllEvents

---
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/EventService.java
@@ -0,0 +1,11 @@
+package com.online.travel.planning.online.travel.planning.backend.Service;
+
+import java.util.List;
+
+import com.online.travel.planning.online.travel.planning.backend.Model.Event;
+
+public interface EventService {
+    List<Event> getAllEvents();
+
+
+}
