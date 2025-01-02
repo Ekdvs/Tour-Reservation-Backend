0c6bb0c14837eda23a9264ed428885b0c50c9668
Ekdvs <ekdvsampath02@gmail.com>
2025-01-02 14:12:51 +0530

    feat: add List import to TravelPlaceController

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/TravelPlaceController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/TravelPlaceController.java
@@ -1,5 +1,7 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
