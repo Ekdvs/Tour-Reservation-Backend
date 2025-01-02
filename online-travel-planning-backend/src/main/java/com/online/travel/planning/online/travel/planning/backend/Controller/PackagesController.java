2112a3e40c33da2e9bd8c95eb302655cd67a4a34
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 14:06:34 +0530

    feat: enable CORS for PackagesController

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/PackagesController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/PackagesController.java
@@ -1,8 +1,10 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class PackagesController {
 
 }
