2b19f904fe6c0857b5c4f0fcb0ffddbf75558aac
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 14:07:09 +0530

    feat: inject PackagesService into PackagesController

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/PackagesController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/PackagesController.java
@@ -1,12 +1,18 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.online.travel.planning.online.travel.planning.backend.Service.PackagesService;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 @RequestMapping("/packages")
 public class PackagesController {
+     @Autowired
+    private PackagesService packagesService;
+
 
 }
