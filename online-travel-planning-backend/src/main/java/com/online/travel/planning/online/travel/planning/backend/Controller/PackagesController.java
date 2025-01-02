514f5d3f07ce0298cb8efa2878a7601629d4f9f9
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 14:07:40 +0530

    feat: add createPackage endpoint to PackagesController

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/PackagesController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/PackagesController.java
@@ -2,10 +2,14 @@ package com.online.travel.planning.online.travel.planning.backend.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.online.travel.planning.online.travel.planning.backend.Model.Packages;
 import com.online.travel.planning.online.travel.planning.backend.Service.PackagesService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -13,6 +17,12 @@ import com.online.travel.planning.online.travel.planning.backend.Service.Package
 public class PackagesController {
      @Autowired
     private PackagesService packagesService;
+
+    @PostMapping("/createPackage")
+    public Packages createPackage(@RequestBody Packages packages) {
+        return packagesService.createPackage(packages);
+    }
+
 
 
 }
