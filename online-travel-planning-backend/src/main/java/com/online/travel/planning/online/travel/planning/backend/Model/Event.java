0c37633698a2858480b5309a5fa6b3d694e6f080
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 12:50:13 +0530

    add primartbke

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/Event.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/Event.java
@@ -1,8 +1,10 @@
 package com.online.travel.planning.online.travel.planning.backend.Model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "event")
 public class Event {
+    @Id
 
 }
