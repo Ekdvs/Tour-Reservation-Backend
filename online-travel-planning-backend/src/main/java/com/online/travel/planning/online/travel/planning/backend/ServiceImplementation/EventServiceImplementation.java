8bf1c2951366fbd0951943eed6a4876fe7562430
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:31:23 +0530

    add searchEventsByName method in EventServiceImplementation

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/EventServiceImplementation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/EventServiceImplementation.java
@@ -161,10 +161,18 @@ String message =
             return eventRepository.save(event);
         }).orElseThrow(() -> new RuntimeException("Event not found with id " + eventId));
     }
+
     @Override
     public void deleteEvent(String eventId) {
         eventRepository.deleteById(eventId);
     }
+
+    @Override
+    public List<Event> searchEventsByName(String name) {
+        return eventRepository.findByEventNameContainingIgnoreCase(name);
+    }
+
+
 
 
     
