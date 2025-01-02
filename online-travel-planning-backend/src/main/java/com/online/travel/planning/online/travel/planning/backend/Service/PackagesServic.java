483e2d9b2b605533d053fff025172e78060c851a
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:54:16 +0530

    add deletePackage method to PackagesServic interface

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/PackagesServic.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/PackagesServic.java
@@ -7,6 +7,8 @@ import com.online.travel.planning.online.travel.planning.backend.Model.Packages;
 public interface PackagesServic {
     Packages createPackage(Packages packages);
     Packages updatePackage(String id, Packages packages);
+    void deletePackage(String id);
+    
     
     
     
