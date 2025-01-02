8540d306e7dc6bbc942988e36eecac37cdfe1422
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:33:05 +0530

    add @RestController annotation to EventController for RESTful API support

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/EventController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/EventController.java
@@ -1,5 +1,9 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class EventController {
 
 }
