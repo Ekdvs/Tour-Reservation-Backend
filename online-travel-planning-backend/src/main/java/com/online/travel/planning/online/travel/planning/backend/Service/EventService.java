c1043b4f06dbd57cf8c98e571388aea6912cc102
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:11:42 +0530

    addEvent

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/EventService.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/EventService.java
@@ -1,7 +1,10 @@
 package com.online.travel.planning.online.travel.planning.backend.Service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import com.online.travel.planning.online.travel.planning.backend.Model.Event;
 
@@ -9,6 +12,7 @@ public interface EventService {
     List<Event> getAllEvents();
     Optional<Event> getEventById(String eventId);
     List<Event> getEventByType(String eventType);
+    Event addEvent(Event event, MultipartFile imagefile)throws IOException;
 
 
 }
