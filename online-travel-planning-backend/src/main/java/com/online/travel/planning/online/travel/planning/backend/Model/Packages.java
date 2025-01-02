53eb81bb7f008a0071865e8ce02d22009d76266f
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:44:18 +0530

    add ID field to Packages model for MongoDB document identification

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/Packages.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/Packages.java
@@ -1,8 +1,12 @@
 package com.online.travel.planning.online.travel.planning.backend.Model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "packages")
 public class Packages {
+    @Id
+    private String id;
+
 
 }
