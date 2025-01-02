1d8ceaab4370c49f523cffcb755adbd3bff0b98d
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-02 13:34:53 +0530

    add @GetMapping annotation to EventController for retrieving all events

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/EventController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/EventController.java
@@ -1,7 +1,10 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +19,7 @@ public class EventController {
 
     private static final String IMAGE_DIRECTORY = "src/main/resources/static/images/";
 
+    @GetMapping("/getAllEvents")
+    
 
 }
