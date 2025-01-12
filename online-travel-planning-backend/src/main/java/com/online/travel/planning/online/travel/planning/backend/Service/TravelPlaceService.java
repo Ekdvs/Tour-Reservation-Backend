affc842b8e8ac460eec2a19a5eee12250eee8646
Ekdvs <ekdvsampath02@gmail.com>
2025-01-12 12:20:32 +0530

    fix: add method to retrieve TravelPlace by ID in TravelPlaceService interface

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/TravelPlaceService.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/TravelPlaceService.java
@@ -1,8 +1,10 @@
 package com.online.travel.planning.online.travel.planning.backend.Service;
 import com.online.travel.planning.online.travel.planning.backend.Model.TravelPlace;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface TravelPlaceService {
     List<TravelPlace> getAllTravelPlaces();
+    Optional<TravelPlace> getTravelPlaceById(String placeId);
 }
