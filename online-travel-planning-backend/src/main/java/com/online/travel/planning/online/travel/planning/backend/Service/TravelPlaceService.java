38444c0825745b8671b0aa92749f2ea046f4c8e2
Ekdvs <ekdvsampath02@gmail.com>
2025-01-12 12:24:13 +0530

    fix: update TravelPlaceService interface to add methods for adding, updating, deleting, and searching TravelPlaces

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/TravelPlaceService.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/TravelPlaceService.java
@@ -1,5 +1,8 @@
 package com.online.travel.planning.online.travel.planning.backend.Service;
 import com.online.travel.planning.online.travel.planning.backend.Model.TravelPlace;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,4 +11,8 @@ public interface TravelPlaceService {
     List<TravelPlace> getAllTravelPlaces();
     Optional<TravelPlace> getTravelPlaceById(String placeId);
     List<TravelPlace> searchTravelPlaceByName(String placeName);
+    TravelPlace addPlace(TravelPlace travelPlace, MultipartFile imagefile)throws IOException;
+    TravelPlace updatePlace(String placeid,TravelPlace travelPlace ,MultipartFile imagefile)throws IOException;
+    void deletePlace(String placeid);
+    List<TravelPlace> searchPlaceByName(String placeName);
 }
