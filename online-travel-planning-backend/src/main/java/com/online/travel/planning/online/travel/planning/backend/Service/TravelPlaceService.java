a5bb170a216a1415778ad8606bb86264ee521d37
Ekdvs <ekdvsampath02@gmail.com>
2025-01-12 12:17:40 +0530

    fix: remove unused methods from TravelPlaceService interface

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/TravelPlaceService.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Service/TravelPlaceService.java
@@ -4,10 +4,5 @@ import java.util.List;
 
 
 public interface TravelPlaceService {
-    TravelPlace createTravelPlace(TravelPlace travelPlace);
-    TravelPlace getTravelPlaceById(String placeId);
-    TravelPlace getTravelPlaceByName(String placeName);
     List<TravelPlace> getAllTravelPlaces();
-    void deleteTravelPlace(String placeName);
-    TravelPlace updateTravelPlace(String placeName, TravelPlace travelPlace);
 }
