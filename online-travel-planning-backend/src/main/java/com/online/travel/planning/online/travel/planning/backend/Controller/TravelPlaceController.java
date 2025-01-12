78bacf7ad13e3258d1982351563ccd01b87e5837
Ekdvs <ekdvsampath02@gmail.com>
2025-01-12 14:06:48 +0530

    feat: enhance TravelPlaceController to support image uploads and improve place management

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/TravelPlaceController.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Controller/TravelPlaceController.java
@@ -1,22 +1,19 @@
 package com.online.travel.planning.online.travel.planning.backend.Controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.online.travel.planning.online.travel.planning.backend.Model.TravelPlace;
 import com.online.travel.planning.online.travel.planning.backend.Service.TravelPlaceService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -25,50 +22,55 @@ public class TravelPlaceController {
     @Autowired
     private TravelPlaceService travelPlaceService;
 
+    private static final String IMAGE_DIRECTORY = "src/main/resources/static/images/";
      //add place
     @PostMapping("/addplace")
-    public ResponseEntity<TravelPlace> addPlace(@RequestBody TravelPlace travelPlace) {
-        TravelPlace savedPlace = travelPlaceService.createTravelPlace(travelPlace);
-        return ResponseEntity.ok(savedPlace);
+    public ResponseEntity<?> addEvent(@RequestPart("place")String placeJson, @RequestPart("imageFile") MultipartFile imagefile) throws IOException {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            TravelPlace place = objectMapper.readValue(placeJson, TravelPlace.class);
+            TravelPlace placeExist = travelPlaceService.addPlace(place,imagefile);
+            return new ResponseEntity<>(placeExist, HttpStatus.CREATED);
+
+
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
     //get all places
     @GetMapping("/allplaces")
     public ResponseEntity<List<TravelPlace>> getAllPlaces() {
         List<TravelPlace> places = travelPlaceService.getAllTravelPlaces();
         return ResponseEntity.ok(places);
     }
+
      //delete place by name
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteTravelPlace(@PathVariable("id") String placename) {
-        travelPlaceService.deleteTravelPlace(placename);
-        return ResponseEntity.ok("place with ID " + placename + " has been deleted successfully.");
+    public ResponseEntity<String> deleteTravelPlace(@PathVariable("id") String placeid) {
+
+        travelPlaceService.deletePlace(placeid);
+        return new ResponseEntity<>("Place deleted", HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<TravelPlace> getPlaceById(@PathVariable String id) {
-        TravelPlace place = travelPlaceService.getTravelPlaceById(id);
-        return place != null ? ResponseEntity.ok(place) : ResponseEntity.notFound().build();
-    }
-    // Get TravelPlace by placeName
-    @GetMapping("/getPlaceByName/{id}")
-    public TravelPlace getPlaceByName(@PathVariable("id") String placeName) {
-        return travelPlaceService.getTravelPlaceByName(placeName);
-    }
 
-    @PutMapping("/update/{name}")
-    public ResponseEntity<TravelPlace> updatePlace(@PathVariable("name") String placeName, @RequestBody TravelPlace travelPlace) {
-         // Ensure that the travelPlace to update has the correct values for the update
-        if (travelPlace == null || placeName == null || placeName.isEmpty()) {
-            return ResponseEntity.badRequest().build(); // Bad Request if input is invalid
-        }
-        // Call the service to update the TravelPlace
-        TravelPlace updatedPlace = travelPlaceService.updateTravelPlace(placeName, travelPlace);
-        // Check if the place was successfully updated or not found
-        if (updatedPlace != null) {
-            return ResponseEntity.ok(updatedPlace);  // Return the updated place with status 200 OK
-        }
-        else {
-            return ResponseEntity.notFound().build();  // Return 404 if the place was not found
+
+
+
+    @PutMapping("/updatePlace/{id}")
+    public ResponseEntity<?> updateEvent(
+            @PathVariable("id") String placeId,
+            @RequestPart("place") String placeJson,
+            @RequestPart(value = "imageFile", required = false) MultipartFile imageFile
+    ) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            TravelPlace updatedPlace = objectMapper.readValue(placeJson, TravelPlace.class);
+            TravelPlace place = travelPlaceService.updatePlace(placeId, updatedPlace, imageFile);
+            return new ResponseEntity<>(place, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
