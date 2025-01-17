4ec2e7a4f673a3062a93c7420c8fcb87b79a8663
Ekdvs <ekdvsampath02@gmail.com>
2025-01-17 06:40:46 +0530

    implementation class add

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/BlogRepository.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/BlogRepository.java
@@ -1,4 +1,8 @@
 package com.online.travel.planning.online.travel.planning.backend.Repository;
 
-public interface BlogRepository {
+import com.online.travel.planning.online.travel.planning.backend.Model.Blog;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.Repository;
+
+public interface BlogRepository extends MongoRepository<Blog,String> {
 }
