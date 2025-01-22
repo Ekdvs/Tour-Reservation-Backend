2c3f66a9b020999591106e054befa72e6e1e61b6
Ekdvs <ekdvsampath02@gmail.com>
2025-01-22 21:19:40 +0530

    fix
    fix

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/EventServiceImplementation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/EventServiceImplementation.java
@@ -138,7 +138,7 @@ String message =
                 "</html>";
                 allUsers.forEach(user -> {
                     if (user.getUserRole().equals("user") || user.getUserRole().equals("GUIDE")) {
-                        emailService.sendEmail(user.getUserEmail(), subject, message);
+                       // emailService.sendEmail(user.getUserEmail(), subject, message);
                     }
                 });
         
