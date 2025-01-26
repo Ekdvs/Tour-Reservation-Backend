448bd5d250b802dc2f5fccdb1eab32601ff8b57e
Ekdvs <ekdvsampath02@gmail.com>
2025-01-26 08:59:42 +0530

    Remove all ignored files from version control

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Model/DailyIncome.java
+++
@@ -1,5 +0,0 @@
-package com.online.travel.planning.online.travel.planning.backend.Model;
-
-public class DailyIncome {
-
-}
--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/Repository/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.online.travel.planning.online.travel.planning.backend.Repository;
-
-import com.online.travel.planning.online.travel.planning.backend.Model.Blog;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface BlogRepository extends MongoRepository<Blog,String> {
-}
