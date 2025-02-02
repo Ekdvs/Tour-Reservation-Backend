a3e67326b9ca165f18f763cc6545fa3814427e99
Ekdvs <ekdvsampath02@gmail.com>
2025-02-02 11:48:00 +0530

    promoteUserToGuide

--- online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/ReservationServiceImplementation.java
+++ online-travel-planning-backend/src/main/java/com/online/travel/planning/online/travel/planning/backend/ServiceImplementation/ReservationServiceImplementation.java
@@ -6,6 +6,10 @@ import com.online.travel.planning.online.travel.planning.backend.Service.Reserva
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ReservationServiceImplementation implements ReservationService {
     @Autowired
@@ -16,4 +20,63 @@ public class ReservationServiceImplementation implements ReservationService {
         
         return reservationRepository.save(reservation);
     }
+
+    @Override
+    public List<Reservation> getAllReservations() {
+        return reservationRepository.findAll();
+    }
+
+    @Override
+    public List<Reservation>getReservationsByUserEmail(String userId){
+        return reservationRepository.findReservationByUserEmail(userId);
+    }
+
+    @Override
+    public Optional<Reservation> getReservationById(String reservationId) {
+        return reservationRepository.findById(reservationId);
+    }
+
+    @Override
+    public Reservation updateReservation(String reservationId, Reservation reservationDetail) {
+        return reservationRepository.findById(reservationId).map(reservation -> {
+            reservation.setPackageId(reservationDetail.getPackageId());
+            reservation.setUserEmail(reservationDetail.getUserEmail());
+            reservation.setReservationDate(reservationDetail.getReservationDate());
+            reservation.setReservationTime(reservationDetail.getReservationTime());
+            reservation.setNumberOfPeople(reservationDetail.getNumberOfPeople());
+            reservation.setTotalPrice(reservationDetail.getTotalPrice());
+            reservation.setStatus(reservationDetail.getStatus());
+            reservation.setTravelGuide(reservationDetail.getTravelGuide());
+            return reservationRepository.save(reservation);
+        }).orElseThrow(() -> new RuntimeException("Reservation not found with id " + reservationId));
+    }
+
+    @Override
+    public void deleteReservation(String reservationId) {
+        reservationRepository.deleteById(reservationId);
+    }
+
+    @Override
+    public double getTotalPriceByCurrentDate() {
+        LocalDate currentDate = LocalDate.now();
+        return reservationRepository.findTotalChargesByDate(currentDate)
+                .stream()
+                .mapToDouble(Reservation::getTotalPrice)
+                .sum();
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return reservationRepository.findAllTotalPrice()
+                .stream()
+                .mapToDouble(Reservation::getTotalPrice)
+                .sum();
+    }
+
+
+
+
+
+
+
 }
